2e68ef87b6aeaf1b8917d43718eb5dd5af2cd70c
GuestSneezeOS Dev <163439609+GuestSneezeOSDev@users.noreply.github.com>
2024-04-25 18:32:58 +0400

    Delete releng directory

--- releng/airootfs/etc/skel/menus.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-
-
-dialog --checklist "pick one" 50 30 20 \
-        1 Pacstrap on \
-        2 "fstab" on \
-        3 chicken off
--- releng/airootfs/usr/local/bin/setfont.sh
+++
@@ -1,2 +0,0 @@
-#!/usr/bin/env bash
-setfont sun12x22
