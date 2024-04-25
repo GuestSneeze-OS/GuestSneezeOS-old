46691470ac49b1732e44b872faa6a8cdb3c0c1c6
GuestSneezeOS Dev <163439609+GuestSneezeOSDev@users.noreply.github.com>
2024-04-25 18:35:36 +0400

    Created GuestSneezeOS 11 FULL Source Code
    Linux GNU Full Source Code Public Blah Blah Blah

---
+++ releng/airootfs/etc/skel/menus.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+
+
+dialog --checklist "pick one" 50 30 20 \
+        1 Pacstrap on \
+        2 "fstab" on \
+        3 chicken off
---
+++ releng/airootfs/usr/local/bin/copyfiles.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+chmod +x /etc/skel/ezarch*
+chmod +x /etc/skel/install
+chmod +x /etc/skel/vol
+cp /etc/skel/ezarch* /root
+cp /etc/skel/install /root
