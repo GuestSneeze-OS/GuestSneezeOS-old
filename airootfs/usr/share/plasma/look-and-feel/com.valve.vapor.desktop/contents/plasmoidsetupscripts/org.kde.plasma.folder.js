487ef699f7f58e26be69b474deb411953f268255
GuestSneezeOS Dev <163439609+GuestSneezeOSDev@users.noreply.github.com>
2024-05-22 17:25:53 +0400

    Add files via upload

---
+++ airootfs/usr/share/plasma/look-and-feel/com.valve.vapor.desktop/contents/plasmoidsetupscripts/org.kde.plasma.folder.js
@@ -0,0 +1,4 @@
+applet.wallpaperPlugin = 'org.kde.image'
+applet.currentConfigGroup = ["Wallpaper", "org.kde.image", "General"]
+applet.writeConfig("Image", "/usr/share/wallpapers/Steam Deck Logo Default.jpg")
+applet.reloadConfig()
