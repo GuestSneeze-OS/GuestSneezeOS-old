12646d10de92b1e425d70b67f918a0715776f76e
GuestSneezeOS Dev <163439609+GuestSneezeOSDev@users.noreply.github.com>
2024-05-30 15:20:40 +0400

    Update customize_airootfs.sh

--- airootfs/root/customize_airootfs.sh
+++ airootfs/root/customize_airootfs.sh
@@ -1,8 +1,2 @@
 #!/bin/bash
-
-cd /root
-git clone https://github.com/GuestSneezeOS-Official/GSOS-install
-cp -R /root/holoiso-installer/* /
-cd -
-chmod +x -R /usr/bin /etc/lib /etc/X11 /home/liveuser/Desktop
 systemctl enable sddm
