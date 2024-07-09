eb7152abff003bd02809ee31585bebb8964624bb
Xaryx <163439609+GuestSneezeOSDev@users.noreply.github.com>
2024-07-09 20:51:11 +0400

    Update gsosuninstall.sh

--- airootfs/etc/gsosinstall/gsosuninstall.sh
+++ airootfs/etc/gsosinstall/gsosuninstall.sh
@@ -1,4 +1,8 @@
-crudini --del /etc/pacman.conf winesapos
+echo "Removing SteamOS Repo"
+# Start Removing SteamOS
 crudini --del /etc/pacman.conf jupiter
 crudini --del /etc/pacman.conf holo
+echo "Removing winesapOS"
+# Start Removing winesapOS repo
+crudini --del /etc/pacman.conf winesapos
 pacman -S -y
